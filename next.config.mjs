a38491dafc6661e796a4cfafd6ee486e29a9201e
andretfw <118443112+andretfw@users.noreply.github.com>
2026-07-23 12:41:29 +0300

    Enforce TypeScript and ESLint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -9,12 +9,6 @@ const nextConfig = {
     ],
     domains: ['images.unsplash.com', 'unsplash.com'],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
