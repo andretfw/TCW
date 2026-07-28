911c83d9e03f6c587220f2080219469f48eeb7a6
andretfw <118443112+andretfw@users.noreply.github.com>
2026-07-28 13:04:40 +0300

    Allow NCI SEER guide images

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         hostname: 'plus.unsplash.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'seer.cancer.gov',
+        pathname: '/i/factsheets/**',
+      },
     ],
   },
 };
