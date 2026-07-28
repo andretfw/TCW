6cb757985afbdeedf0f0a7a7b328e3b8db8c0f9e
andretfw <118443112+andretfw@users.noreply.github.com>
2026-07-28 13:41:16 +0300

    Use a hero-ready leukemia cell image

--- lib/cancer-data.ts
+++ lib/cancer-data.ts
@@ -28,7 +28,7 @@ export const cancerTypes = [
   },
   { id: 'skin', icon: 'sun', image: 'https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?w=800', contentImage: 'https://images.unsplash.com/photo-1581594693702-fbdc51b2763b?w=800' },
   { id: 'kidney', icon: 'activity', image: 'https://images.unsplash.com/photo-1631549916768-4119b2e5f926?w=800', contentImage: 'https://images.unsplash.com/photo-1631217868264-e5b90bb7e133?w=800' },
-  { id: 'leukemia', icon: 'droplet', image: '/images/leukemia-blood-cell-development.jpg', contentImage: '/images/leukemia-blood-cell-development.jpg' },
+  { id: 'leukemia', icon: 'droplet', image: '/images/leukemia-cells.jpg', contentImage: '/images/leukemia-blood-cell-development.jpg' },
   { id: 'liver', icon: 'activity', image: 'https://images.unsplash.com/photo-1579165466741-7f35e4755660?w=800', contentImage: '/images/liver-anatomy.jpg' },
   { id: 'pancreatic', icon: 'zap', image: '/images/pancreatic-cancer-anatomy.jpg', contentImage: '/images/pancreatic-cancer-anatomy.jpg' },
   { id: 'ovarian', icon: 'circle', image: 'https://images.unsplash.com/photo-1559757148-5c350d0d3c56?w=800', contentImage: 'https://images.unsplash.com/photo-1631217868264-e5b90bb7e133?w=800' },
