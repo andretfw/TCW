c8c29f703957e1c9a46c63f829f078d304d65e15
andretfw <118443112+andretfw@users.noreply.github.com>
2026-07-29 12:51:57 +0300

    Add standard page SEO metadata [skip netlify]

---
+++ app/[locale]/bienestar-emocional/layout.tsx
@@ -0,0 +1,9 @@
+import {
+  createStandardPageMetadata,
+  StandardPageLayout,
+} from '@/lib/standard-page-layout';
+
+export const generateMetadata = createStandardPageMetadata(
+  'emotionalWellBeing',
+);
+export default StandardPageLayout;
