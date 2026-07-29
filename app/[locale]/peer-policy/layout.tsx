bc845fb0ea1277c57c15d348eef4b9c299e0f1dd
andretfw <118443112+andretfw@users.noreply.github.com>
2026-07-29 12:52:48 +0300

    Add standard page SEO metadata [skip netlify]

---
+++ app/[locale]/peer-policy/layout.tsx
@@ -0,0 +1,7 @@
+import {
+  createStandardPageMetadata,
+  StandardPageLayout,
+} from '@/lib/standard-page-layout';
+
+export const generateMetadata = createStandardPageMetadata('peerPolicy');
+export default StandardPageLayout;
