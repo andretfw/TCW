f3facc5c2be9555a4c1aa028c2a035e0fbfdaa77
andretfw <118443112+andretfw@users.noreply.github.com>
2026-07-29 12:52:51 +0300

    Add standard page SEO metadata [skip netlify]

---
+++ app/[locale]/preguntas-doctor/layout.tsx
@@ -0,0 +1,9 @@
+import {
+  createStandardPageMetadata,
+  StandardPageLayout,
+} from '@/lib/standard-page-layout';
+
+export const generateMetadata = createStandardPageMetadata(
+  'questionsForDoctor',
+);
+export default StandardPageLayout;
