d7e0b871fd42d13626780ed41670dddf6baa1f3f
andretfw <118443112+andretfw@users.noreply.github.com>
2026-07-29 12:52:06 +0300

    Add standard page SEO metadata [skip netlify]

---
+++ app/[locale]/entender-diagnostico/layout.tsx
@@ -0,0 +1,9 @@
+import {
+  createStandardPageMetadata,
+  StandardPageLayout,
+} from '@/lib/standard-page-layout';
+
+export const generateMetadata = createStandardPageMetadata(
+  'understandingDiagnosis',
+);
+export default StandardPageLayout;
